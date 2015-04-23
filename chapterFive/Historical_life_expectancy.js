a7dff8e7739bae62d5e9bc336d145c1c24e522e6
Reece <lrv378@gmail.com>
2015-04-23 09:13:06 -0600

    Finish Historical Life Expectancy

--- chapterFive/Historical_life_expectancy.js
+++ chapterFive/Historical_life_expectancy.js
@@ -3,13 +3,37 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 
-
+var sixteenArray = [],
+    seventeenArray = [],
+    eighteenArray = [],
+    nineteenArray = [],
+    twentyArray = [],
+    twentyOneArray = [];
 function arrayPush(array){
   for (var i = 0; i < array.length; i++){
-    console.log(array[i]);
+    if ((Math.ceil(array[i].died/100)) === 16) {
+      sixteenArray.push((array[i].died)-(array[i].born));
+    } else if ((Math.ceil(array[i].died/100)) === 17) {
+      seventeenArray.push((array[i].died)-(array[i].born));
+    } else if ((Math.ceil(array[i].died/100)) === 18) {
+  		eighteenArray.push((array[i].died)-(array[i].born));
+    } else if ((Math.ceil(array[i].died/100)) === 19) {
+  		nineteenArray.push((array[i].died)-(array[i].born));
+    } else if ((Math.ceil(array[i].died/100)) === 20) {
+  		twentyArray.push((array[i].died)-(array[i].born));
+    } else if ((Math.ceil(array[i].died/100)) === 21) {
+  		twentyOneArray.push((array[i].died)-(array[i].born));
+    }
   }
+  console.log(Math.floor(average(sixteenArray)*10)/10);
+  console.log(Math.floor(average(seventeenArray)*10)/10);
+  console.log(Math.floor(average(eighteenArray)*10)/10);
+  console.log(Math.floor(average(nineteenArray)*10)/10);
+  console.log(Math.floor(average(twentyArray)*10)/10);
+  console.log(Math.floor(average(twentyOneArray)*10)/10;
 }
 
+arrayPush(ancestry);
 // Your code here.
 
 // → 16: 43.5
